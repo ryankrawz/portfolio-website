5ece62b226bb574d269201019b8d27f5ededcdf9
ryankrawz <krawczry@bc.edu>
2021-02-19 17:32:41 -0500

    Function skeleton

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+function renderNavBar(sections) {
+    // Render nav bar
+}
+
+function renderAbout(about) {
+    // Render about section
+}
+
+function renderExperiences(experiences) {
+    // Render experiences section
+}
+
+function renderProjects(projects) {
+    // Render projects section
+}
+
+function renderTravels(travels) {
+    // Render travels section
+}
+
+// Render nav bar and all sections of page
+function renderMainPage(data) {
+    document.querySelector('main').innerHTML = `
+        ${renderNavBar(Object.keys(data.body))}
+        ${renderAbout(data.body.about)}
+        ${renderExperiences(data.body.experiences)}
+        ${renderProjects(data.body.projects)}
+        ${renderTravels(data.body.travels)}
+    `;
+}
+
+// Hide or display element based on current state
 function toggleVisibility(id) {
     let target = document.getElementById(id);
     if (target.style.display === 'block') {
@@ -7,7 +39,7 @@ function toggleVisibility(id) {
     }
 }
 
+// Page loading process
 fetch('data.json').then(response => response.json()).then(data => {
-    console.log(data);
-	// render HTML here
+    renderMainPage();
 });
